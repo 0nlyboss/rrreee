a1e78031ae3c3efbf9d9eb150cedfdca8f3a4678
0nlyboss <100768418+0nlyboss@users.noreply.github.com>
2025-10-21 18:52:27 +0100

    Remove theme initialization code

--- asd.lua
+++ asd.lua
@@ -5940,13 +5940,5 @@
 -- 
 
 
--- Initialisation stuff
-library:config_list_update()
-for index, value in themes.preset do 
-    pcall(function()
-        library:update_theme(index, value)
-    end)
-end
-task.wait()
-library.old_config = library:get_config()
+
 
